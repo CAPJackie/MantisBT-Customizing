c19b5cbcce603a9bbe6d6fc64b47cef7df9f2477
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2019-05-16 17:34:57 -0500

    Updating string

--- config/custom_strings_inc.php
+++ config/custom_strings_inc.php
@@ -8,20 +8,20 @@ switch( $g_active_language ) {
                 $s_reproducibility = 'Reproducible';
                 $s_reproducibility_enum_string = '10:si,20:no';
 				
-                $g_access_levels_enum_string = '20:desarrollador, 30:consultor de tests,40:ingeniero de tests,50:lider del equipo de tests,60:administrador de tests,90:ingeniero de ambiente de tests';
+                $s_access_levels_enum_string = '20:desarrollador, 30:consultor de tests,40:ingeniero de tests,50:lider del equipo de tests,60:administrador de tests,90:ingeniero de ambiente de tests';
 
 
 
 		break;
 	default: # english
-                $g_severity_enum_string = '10:minor,20:major,30:critical';
+                $s_severity_enum_string = '10:minor,20:major,30:critical';
                 $s_priority_enum_string = '10:deferred,20:delayed,30:immediate';
 
                 $s_email_reproducibility = 'Reproducible';
                 $s_reproducibility = 'Reproducible';
                 $s_reproducibility_enum_string = '10:yes,20:no';
 				
-                $g_access_levels_enum_string = '20:developer, 30:test consultant,40:test engineer,50:test team leader,60:test manager,90:test environment engineer';
+                $s_access_levels_enum_string = '20:developer, 30:test consultant,40:test engineer,50:test team leader,60:test manager,90:test environment engineer';
 
 
                 break;
